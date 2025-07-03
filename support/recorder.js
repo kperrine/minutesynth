9a44dd1f660ad60614c8f563e77461547257c2c7
Ken Perrine <academiken@gmail.com>
2025-07-02 21:58:39 -0500

    Code cleanup, less default namespace pollution

--- support/recorder.js
+++ support/recorder.js
@@ -4,7 +4,7 @@ function record8(voiceName, voiceFunc, sampleRate=16574, dur=3.95, noteFreq, not
   console.log('Beginning rendering.')
   var R = sampleRate
   let oac = new OfflineAudioContext(1, ~~(R * dur), R)
-  let synth = M$(oac)
+  let synth = MinuteSynth(oac)
 
   let voice = voiceFunc(synth)
   let now = synth.now()
@@ -35,16 +35,16 @@ function record8(voiceName, voiceFunc, sampleRate=16574, dur=3.95, noteFreq, not
   
 function _makeLink(samples, voiceName, buf, sampleRate, dur) {
   // Provide buffer for playback and play it:
-  let playBuf = m$.ac.createBuffer(1, ~~(sampleRate * dur), 27928/*sampleRate*/)
+  let playBuf = synthInst.ac.createBuffer(1, ~~(sampleRate * dur), 27928/*sampleRate*/)
   let srcData = buf.getChannelData(0)
   let data = playBuf.getChannelData(0)
   for (let i = 0; i < data.length; i++) {
     data[i] = srcData[i]
   }
-  let playBufNode = m$.ac.createBufferSource()
+  let playBufNode = synthInst.ac.createBufferSource()
   playBufNode.buffer = playBuf
   playBufNode.loop = false
-  playBufNode.connect(m$.ac.destination)
+  playBufNode.connect(synthInst.ac.destination)
   playBufNode.connect(analyser) // Connect playback to analyzer. Not with a "z".
   playBufNode.start()
 
